e9db33e98cd3e08b22356b5858ce8808144a7a1d
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2020-06-05 11:22:54 +0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-/xmrig/build/xmrig -a rx/0 -o stratum+tcp://randomxmonero.eu.nicehash.com:3380 -u 3GEXLmtmfzFdyhQf77Yogj7H2cqDe7tU84 -p x --donate-level=1
+/xmrig/build/xmrig -a rx/0 -o stratum+tcp://randomxmonero.eu.nicehash.com:3380 -u 3GEXLmtmfzFdyhQf77Yogj7H2cqDe7tU84.$R -p x --donate-level=1
